2542c8ed9d3ff67aa3ad5329ddea4c0aab05b6d0
andreas hrasdin <andreas.hrasdin@gmx.at>
2021-03-12 15:00:57 +0100

    started work

---
+++ TourPlaner_andreas/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace TourPlaner_Andreas
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _output = "Hello World!";
+        private string _input;
+
+        public string Input
+        {
+            get
+            {
+                Debug.Print("read Input");
+                return _input;
+            }
+            set
+            {
+                Debug.Print("write Input");
+                if (Input != value)
+                {
+                    Debug.Print("set Input-value");
+                    _input = value;
+
+                    // it does not work to fire an event from outside in C#
+                    // can be achieved by creating a method like "RaiseCanExecuteChanged".
+                    // this.ExecuteCommand.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+                    // this triggers the UI and the ExecuteCommand
+                    Debug.Print("fire propertyChanged: Input");
+                    OnPropertyChanged(nameof(Input));
+                }
+            }
+        }
+
+        public string Output
+        {
+            get
+            {
+                Debug.Print("read Output");
+                return _output;
+            }
+            set
+            {
+                Debug.Print("write Output");
+                if (_output != value)
+                {
+                    Debug.Print("set Output");
+                    _output = value;
+                    Debug.Print("fire propertyChanged: Output");
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand ExecuteCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            Debug.Print("ctor MainViewModel");
+            this.ExecuteCommand = new ExecuteCommand(this);
+
+            #region Simpler Solution
+
+            // Alternative: https://docs.microsoft.com/en-us/archive/msdn-magazine/2009/february/patterns-wpf-apps-with-the-model-view-viewmodel-design-pattern#id0090030
+            // this.ExecuteCommand = new RelayCommand(() => Output = $"Hello {Input}!");
+
+            #endregion
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Print($"propertyChanged \"{propertyName}\"");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- TourPlaner_andreas/MainWindow.xaml.cs
+++ TourPlaner_andreas/MainWindow.xaml.cs
@@ -24,5 +24,7 @@ namespace TourPlaner_andreas
         {
             InitializeComponent();
         }
+      
+        
     }
 }
